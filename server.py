5b74621b5d89dfa0538c2523497de9d2be1533e6
kubotashu0421@gmail.com <kubotashu0421@gmail.com>
2021-12-10 21:55:16 +0900

    serverの処理を追加

--- server.py
+++ server.py
@@ -1,28 +1,34 @@
 from flask import Flask
 from flask_restful import Api, Resource, abort, reqparse
+import pickle
+from main import LogisticRegressionGD
 
 class Server(Resource):
   def get(self):
-    return {"name": "Hello {}!".format(args["name"]), "age": args["age"]}
-
-  def post(self):
+    # リクエストパラメータをparse
     parser = reqparse.RequestParser()
-    parser.add_argument(
-      "q_param", type=int, location="args", help="q_param is numbers"
-    )
-    parser.add_argument("f_param", location="form")
+    parser.add_argument("petal_length", type=float, required=True, help="petal_length required!")
+    parser.add_argument("petal_width", type=float, required=True, help="petal_width required!")
     args = parser.parse_args()
-    return {
-      "post": "Hello World!",
-      "qParam": args["q_param"],
-      "fParam": args["f_param"],
-    }
+
+    # 前処理
+    input = model.input_fn(args)
+
+    # 推論
+    _output = model.predict(input)
+
+    # 後処理
+    output = model.output_fn(_output)
+    
+    return { "catetory": output }
 
 app = Flask(__name__)
 api = Api(app)
-
-
 api.add_resource(Server, "/")
 
 if __name__ == "__main__":
+  # 学習済modelの読み込み
+  with open('estimator.pickle', mode='rb') as f:
+    model = pickle.load(f)
+
   app.run(host='0.0.0.0', debug=True, port=5000)
